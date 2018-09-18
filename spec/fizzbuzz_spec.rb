63e0a5e82035be43eba99ce327e76b04f3e61bff
Raylene <smith.raylene@gmail.com>
2018-09-18 13:27:35 +0100

    updated test & code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,11 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
 
   it 'returns "fizz" when given 3' do
-    expect(fizzbuzz(3)).eq to("fizz")
+    expect(fizzbuzz(3)).to eq("fizz")
+  end
+
+  it 'returns "buzz" when given given 5' do
+  	expect(fizzbuzz(5)).to eq ("buzz")
   end
 
 end
